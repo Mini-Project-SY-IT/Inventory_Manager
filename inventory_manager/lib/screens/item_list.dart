074f9f715aa0076c49359548da03865a34c294e9
Shamshadz <chaudhary.s.shamshad07@gmail.com>
2023-02-25 19:22:44 +0530

    update

---
+++ inventory_manager/lib/screens/item_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemList extends StatefulWidget {
+  const ItemList({super.key});
+
+  @override
+  State<ItemList> createState() => _ItemListState();
+}
+
+class _ItemListState extends State<ItemList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

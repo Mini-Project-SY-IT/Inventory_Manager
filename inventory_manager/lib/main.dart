d1e128a8f8bd518424454c5a197d6b6b4d91538e
chinmaymalkar <chinmaymalkar23@gmail.com>
2023-02-27 16:16:31 +0530

    chinmay

--- inventory_manager/lib/main.dart
+++ inventory_manager/lib/main.dart
@@ -1,21 +1,146 @@
 import 'package:flutter/material.dart';
-import 'package:inventory_manager/screens/Homepage.dart';
-import 'package:inventory_manager/screens/drawer.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(const MyApp());
 }
 
-class Myapp extends StatefulWidget {
-  const Myapp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<Myapp> createState() => _MyappState();
-}
-
-class _MyappState extends State<Myapp> {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Homepage();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    TabController tabController=TabController(length: 2, vsync: this);
+    return Scaffold(
+
+      body: Column(children: [
+        SizedBox(height: 50,),
+        Text("Transaction page",style: TextStyle(fontSize: 30.0),),
+        SizedBox(height: 10,),
+        Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),),
+          elevation: 5,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: TabBar(
+              indicator: BoxDecoration(
+                color: Colors.orange,
+
+              ),
+              controller: tabController,
+              isScrollable: true,
+              labelPadding: EdgeInsets.symmetric(horizontal: 30),
+              tabs: [
+                Tab(child: Text('incoming'),),
+                Tab(child: Text('outgoing'),),
+              ],
+            ),
+          ),
+        ),
+        Expanded(child:TabBarView(
+          controller: tabController,
+          children: [
+            ListView.builder(itemBuilder:(context,index){
+              return Card(
+                elevation: 2.0,
+                shape: BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+
+                child: InkWell(onLongPress: (){},
+                  child: ListTile(
+                    leading: CircleAvatar(child: Icon(Icons.upgrade_sharp,color: Colors.green,)),
+                    title: Text('chinmay'),
+                    subtitle:Text('23/2/2003'),
+                    //Text((DateFormat('yMMMd').format(DateTime.now())),),
+                    // ignore: prefer_interpolation_to_compose_strings
+                    trailing: Text('1100'),
+                  ),
+                ),
+              );
+
+            },itemCount: 5,),
+            ListView.builder(itemBuilder:(context,index){
+              return Card(
+                elevation: 2.0,
+                shape: BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+
+                child: InkWell(onLongPress: (){},
+                  child: ListTile(
+                    leading: CircleAvatar(child: Icon(Icons.upgrade_sharp,color: Colors.green,)),
+                    title: Text('chinmay'),
+                    subtitle:Text('23/2/2003'),
+                    //Text((DateFormat('yMMMd').format(DateTime.now())),),
+                    // ignore: prefer_interpolation_to_compose_strings
+                    trailing: Text('1100'),
+                  ),
+                ),
+              );
+
+            },itemCount: 5,),
+          ],
+        ))
+      ],
+      ),
+      floatingActionButton: IconButton(icon: CircleAvatar(child: Icon(Icons.add),radius: 50.0,),iconSize: 50,onPressed: (){},),
+    );
   }
 }

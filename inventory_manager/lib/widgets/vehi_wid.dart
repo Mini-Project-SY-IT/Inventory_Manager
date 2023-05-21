5d30de98896df41691b711c24e8260e96b32185c
Om21610018 <omkarpenshanwar346@gmail.com>
2023-05-21 11:16:15 +0530

    version 5.0

---
+++ inventory_manager/lib/widgets/vehi_wid.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../screens/Detailed page/Items.dart';
+
+class VehicleWidget extends StatelessWidget {
+  // final String vehicle;
+  final Map<String, dynamic> vehicle;
+
+  const VehicleWidget({Key? key, required this.vehicle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return ItemPage(
+                    vehicle: vehicle['vehicle_name'],
+                    wheeler: vehicle['wheeler'],
+                  );
+                }));
+              },
+              leading: Icon(Icons.precision_manufacturing_outlined),
+              title: Text(vehicle['vehicle_name']),
+              subtitle: Text(vehicle['wheeler']),
+              trailing: Icon(Icons.arrow_forward),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
